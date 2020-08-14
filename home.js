51287820e216fa98d9dea02506f60c946c4b0869
kounaien <1127fujihiroto@gmail.com>
2020-08-14 18:04:00 +0900

    first commmit

---
+++ home.js
@@ -0,0 +1,75 @@
+// console.log("hello");
+// // alert("hello");
+// //youtube 8 hours course
+
+// var b = 'soneting';
+// console.log(b);
+
+// // var age = prompt("what is your age?");
+
+// // document.getElementById("sometax").innerHTML = age;
+
+// var num1 = 10;
+// // increment num1 by 1
+// num1++;
+
+// // decrement num1 by 1
+// num1--;
+
+// console.log(num1);
+
+// //divide / multiply *, remainder %,
+// console.log(num1 % 6);
+
+// // increment/decrement by 10
+// num1 += 10;
+// console.log(num1);
+
+// /*funcitons
+// 1.create a function
+// 2.call the funciton
+// */
+
+// //crate
+// function fun(){
+//     console.log("this is a function");
+// }
+ 
+// //call
+// fun();
+
+/* let's create a function that take in a name
+and says hello followed by your name
+
+for  example
+
+name: "qazi"
+return: "hello qazi"
+*/
+
+// function greeting(yourname){
+//     var result = 'hello ' + ' ' + yourname; // string concatnation
+//     console.log(result);
+// }
+
+// var name = prompt('what is your name?');
+// greeting(name);
+
+
+// how do arguments work in funciton?
+// how do we add 2 numbers together in a fuction?
+
+// function sumNumbers(num1, num2){
+//     var result = num1 + num2;
+//     console.log(result);
+// }
+
+// sumNumbers(10, '10');
+
+
+
+
+
+
+
+
