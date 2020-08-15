38f84198cd631bb452f72fb1b723d2afe4ddba86
kounaien <1127fujihiroto@gmail.com>
2020-08-15 19:03:38 +0900

    deal with json

---
+++ home2.js
@@ -0,0 +1,57 @@
+//objects in javascript
+// dictionaries in python
+
+// let student = {
+//     first: 'Rafeh', 
+//     last: 'Qazi', 
+//     age: 25, 
+//     height: 170,
+//     studentInfo: function(){
+//         return this.first + '\n' + this.last + '\n' +  this.age;
+//     }
+// };
+
+// // console.log(student.first);
+// // console.log(student.last);
+// // student.first = 'notRafeh'; //change value
+// // console.log(student.first);
+// student.age++;
+// console.log(student.age);
+
+// console.log(student.studentInfo());
+
+// // Conditionals, Control flows (if else)
+// // 18-35 is my target demographic
+// // && and
+// // || or
+
+// let age = 45;
+// if ((age >= 18) && (age <= 35) ) {
+//     status = 'target demo';
+//     console.log(status);
+// } else {
+//     status = 'not my audience'
+//     console.log(status);
+// }
+
+// //switch statement
+// //defferentiate between weekday vs. weekend
+// /// day 0 sunday
+// // say 6 is saturday
+// // say 4 is thursday -> weekday
+// switch (2){
+//     case 0:
+//         text = 'sunday';
+//         break;
+//     case 5:
+//         text = 'weekend';
+//         break;
+//     case 6:
+//         text = 'weekend';
+//         break;
+//     default: 
+//         text = 'weekday'
+// }
+
+// console.log(text);
+
